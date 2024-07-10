da8be103264d7d077dde5b2c95c02d0e6053474c
Stuti Srivastava <stsrivastava@microsoft.com>
2024-07-10 19:37:06 +0530

    Enable permission check, and status check in onboarding script

--- Onboarding/Connector/Scripts/create-impact-reporting-connector.sh
+++ Onboarding/Connector/Scripts/create-impact-reporting-connector.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 # Running in debug mode
 # Uncomment to enable debug mode. Make sure you also uncomment set +x in the end of the script.
@@ -6,6 +6,8 @@
 
 # set -x
 set -e
+
+ALERTS_READER_ROLE_NAME="Azure-Alerts-Reader-Role"
 
 print_help () 
 {
@@ -135,33 +137,32 @@ setup_permissions_for_alerts_reading()
 {
   subscription_id=$1
   log "Setting up permissions for alerts reading" 
-  role_name="Azure-Alerts-Reader-Role"
   connector_app_name="AzureImpactReportingConnector"
 
-  role_id=$(az role definition list -n "$role_name"  --query [].id -o tsv)
+  role_id=$(az role definition list -n "$ALERTS_READER_ROLE_NAME"  --query [].id -o tsv)
   if [ ! "$role_id" ]; then
     log "Creating custom role for alerts reading."
     
     custom_role_json_file="/tmp/custom-role.json"
-    create_custom_role_json_file "$subscription_id" "$role_name" "$custom_role_json_file"
+    create_custom_role_json_file "$subscription_id" "$ALERTS_READER_ROLE_NAME" "$custom_role_json_file"
     role_id=$(az role definition create --role-definition "$custom_role_json_file" | jq -r ".id")
     rm -rf "$custom_role_json_file"
     
-    log "Custom role: $role_name for alerts reading created successfully with role id: $role_id."
+    log "Custom role: $ALERTS_READER_ROLE_NAME for alerts reading created successfully with role id: $role_id."
   else
-    log "Custom role: $role_name for alerts reading already exists with role id: $role_id."
+    log "Custom role: $ALERTS_READER_ROLE_NAME for alerts reading already exists with role id: $role_id."
   fi
   
-  log "Assigning the custom role: $role_name to the connector app: $connector_app_name"
+  log "Assigning the custom role: $ALERTS_READER_ROLE_NAME to the connector app: $connector_app_name"
   connector_principal_id=$(az ad sp list --display-name "$connector_app_name" --query "[].id" -o tsv)
 
   assigned_custom_role=$(az role assignment list --assignee "$connector_principal_id" --role "$role_id" --query [].id -o tsv)
 
   if [ ! "$assigned_custom_role" ]; then
     az role assignment create --role "$role_id" --assignee-object-id "$connector_principal_id" --scope "/subscriptions/$subscription_id"
-    log "Custom role: $role_name assigned to the connector app: $connector_app_name"
+    log "Custom role: $ALERTS_READER_ROLE_NAME assigned to the connector app: $connector_app_name"
   else
-    log "Custom role: $role_name already assigned to the connector app: $connector_app_name"
+    log "Custom role: $ALERTS_READER_ROLE_NAME already assigned to the connector app: $connector_app_name"
   fi
 
   log "Successfully setup permissions for alerts reading" 
@@ -183,14 +184,26 @@ create_connector()
   attempt_count=1
   sleep_duration=10
 
-  while ! az rest --method put --url "$url" --body "$request_body" --query "properties.provisioningState" | grep -q "Succeeded"; do
-    sleep_duration=$((sleep_duration*2))
-    log "Attempt #$attempt_count: Connector creation is in progress. Waiting for $sleep_duration seconds before retrying."
-    sleep "$sleep_duration"
-    attempt_count=$((attempt_count+1))
+  while true; do
+    output=$(az rest --method put --url "$url" --body "$request_body" 2>&1 || true)
+    if echo "$output" | grep "Conflict"; then
+      YELLOW='\033[0;33m'
+      echo -e "${YELLOW}A connector is already deployed in this subscription. A new connector will not be deployed."
+      tput sgr0
+      break;
+    elif echo "$output" | grep "InvalidResourceType"; then
+      sleep_duration=$((sleep_duration*2))
+      log "Attempt #$attempt_count: Connector creation is in progress. Waiting for $sleep_duration seconds before retrying."
+      sleep "$sleep_duration"
+      attempt_count=$((attempt_count+1))
+    elif echo "$output" | jq -r .properties.provisioningState | grep -q "Succeeded"; then
+      log "Creation of connector: $connector_name is complete" 
+      break;
+    else
+      log "Failed to create connector: $connector_name due to: $output" 
+    fi
   done 
 
-  log "Creation of connector: $connector_name is complete" 
 }
 
 validate_input()
@@ -210,6 +223,64 @@ validate_input()
   log "==== All required inputs are present ===="
 }
 
+has_role_assignment()
+{
+  user_principal_name=$1
+  role_name=$2
+
+  role_definition_name=$(az role assignment list --assignee "$user_principal_name" --query "[?roleDefinitionName=='$role_name' ]" --include-groups --include-inherited --include-classic-administrators | jq -r .[0].roleDefinitionName)
+  if [ "$role_definition_name" == "$role_name" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+check_role_assignment_for_setting_up_permissions()
+{
+  subscription_id=$1
+  user_principal_name=$2
+
+  role_id=$(az role definition list -n "$ALERTS_READER_ROLE_NAME"  --query [].id -o tsv)
+  if [ ! "$role_id" ]; then
+    log "Custom role: $ALERTS_READER_ROLE_NAME for alerts reading does not exist. Checking for built-in role: User Access Administrator role assignment as that is required for custom role creation"
+    if ! has_role_assignment "$user_principal_name" "User Access Administrator" ; then
+      log "You are not a 'User Access Administrator' on the subscription: $subscription_id. Please get the role assigned to proceed"
+      exit 1
+    fi
+  fi
+
+  if ! has_role_assignment "$user_principal_name" "Role Based Access Administrator" && ! has_role_assignment "$user_principal_name" "User Access Administrator" ; then
+    log "You are neither a 'Role Based Access Administrator' nor a 'User Access Administrator' on the subscription: $subscription_id which is required to assign alert reading permission to the connectors app. Please get the role assigned to proceed"
+    exit 1
+  fi
+}
+
+check_role_assignment_for_deployment()
+{
+  subscription_id=$1
+  user_principal_name=$2
+
+  if ! has_role_assignment "$user_principal_name" "Contributor"; then
+      log "You are not a 'Contributor' on the subscription: $subscription_id which is required to create the connector on your subscription. Please get the role assigned to proceed."
+      exit 1
+    fi   
+}
+
+verify_permissions()
+{
+  subscription_id=$1
+  user_principal_name=$2
+  log "Verifying permissions for the subscription: $subscription_id"
+
+  if has_role_assignment "$user_principal_name" "Owner" ; then
+    log "Permissions are already set for the subscription: $subscription_id"
+  else
+    check_role_assignment_for_setting_up_permissions "$subscription_id" "$user_principal_name" 
+    check_role_assignment_for_deployment "$subscription_id" "$user_principal_name" 
+  fi
+}
+
 create_impact_reporting_connectors()
 {
   log "==== Creating impact reporting connector(s) ===="
@@ -222,12 +293,15 @@ create_impact_reporting_connectors()
       # Create an array with a single element
       subscription_ids=("$subscription_id")
   fi
+  
+  signed_in_user=$(az ad signed-in-user show)
+  user_principal_name=$(echo "$signed_in_user" | jq -r .userPrincipalName)
 
-  # Print the array
   for current_subscription_id in "${subscription_ids[@]}"; do
       az_login_with_prompt "$current_subscription_id"
-      register_feature "Microsoft.Impact" "AzureImpactReportingConnector"
-      register_feature "Microsoft.Impact" "AllowImpactReporting"
+      # verify_permissions "$current_subscription_id" "$user_principal_name"
+      # register_feature "Microsoft.Impact" "AzureImpactReportingConnector"
+      # register_feature "Microsoft.Impact" "AllowImpactReporting"
       setup_permissions_for_alerts_reading "$current_subscription_id"
       create_connector "$current_subscription_id"
       log "==== Impact reporting connector is successfully created on your subscription: $current_subscription_id!! ===="
